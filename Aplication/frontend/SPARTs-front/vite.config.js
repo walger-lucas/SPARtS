169dc65850dfbb39373256b5beb671d5057524d9
IanMSIshikawa <ianishikawa1@gmail.com>
2025-10-22 23:17:33 -0300

    projeto criado

--- Aplication/frontend/SPARTs-front/vite.config.js
+++ Aplication/frontend/SPARTs-front/vite.config.js
@@ -4,11 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import Components from 'unplugin-vue-components/vite';
+import {PrimeVueResolver} from '@primevue/auto-import-resolver';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      resolvers: [PrimeVueResolver()],
+    }),
   ],
   resolve: {
     alias: {
